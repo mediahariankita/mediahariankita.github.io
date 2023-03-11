c3bb06736cea47afefcdfadec8000ed056c0963e
mediahariankita <127623640+mediahariankita@users.noreply.github.com>
2023-03-12 03:01:25 +0800

    Add files via upload

---
+++ themes/newkarma/js/owl-carousel-widget8a54.js
@@ -0,0 +1,1 @@
+var $=jQuery.noConflict();(function($){"use strict";$(document).ready(function(){$(".gmr-widget-carousel").owlCarousel({items:1,pagination:true,autoHeight:true,navigation:false,itemsDesktop:[1000,1],itemsDesktopSmall:[900,1],itemsTablet:[600,1],itemsMobile:[420,1]});});})(jQuery);
